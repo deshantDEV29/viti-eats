69deb7cdfcd6d3c3d1b736875a856e5af69aaeb3
Deshant Singh <89639206+deshantDEV29@users.noreply.github.com>
2022-05-22 23:34:56 +1200

    Menu Page Working
    Restaurant image add needs debugging in admin panel

--- viti-eats-frontend/src/home_component/Menu.js
+++ viti-eats-frontend/src/home_component/Menu.js
@@ -4,17 +4,20 @@ import Category from "./Category";
 import ReactSpinner from "../ReactSpinner";
 import { useLocation } from "react-router-dom";
 import { Link, useNavigate } from "react-router-dom";
+import RestaurantCategories from "./RestaurantCategories";
 
 function Menu() {
   const location = useLocation();
 
   const [data, setData] = useState([]);
+  const [fooddata, setFoodData] = useState([]);
   const navigate = useNavigate();
   const [isLoading, setLoading] = useState(true);
+  let id = location.state.id;
 
   useEffect(() => {
-    let id = location.state.id;
     let restaurantid = { id };
+    console.log(restaurantid);
     async function fetchdata() {
       let result = await fetch(
         "http://localhost:8000/api/getrestaurantdetails",
@@ -27,18 +30,30 @@ function Menu() {
           },
         }
       );
-      result = await result.json();
-      console.log(result)
-      setData(result);
-      setLoading(false);
 
-      console.log(data);
+      result = await result.json();
+      setData(result);
+      console.log("restaurant", data);
+
+      let result1 = await fetch("http://localhost:8000/api/getrestaurantfood", {
+        method: "POST",
+        body: JSON.stringify(restaurantid),
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      });
+
+      result1 = await result1.json();
+      setFoodData(result1);
+      console.log("fooditem", fooddata);
+
+      setLoading(false);
     }
     fetchdata();
   }, []);
 
   const DisplayData = data.map((restaurant_details) => {
-    
     return (
       <RestaurantInfo
         id={restaurant_details.id}
@@ -48,15 +63,48 @@ function Menu() {
     );
   });
 
+  const DisplayfoodData = fooddata.map((fooditem) => {
+    return <Category Id={fooditem.id} Category_Name={fooditem.name} />;
+  });
+
+  function groupBy(data, key) {
+    return data.reduce(function (acc, item) {
+      (acc[item[key]] = acc[item[key]] || []).push(item);
+      return acc;
+    }, {});
+  }
+
+  let groupedfooddata = groupBy(fooddata, "category_description");
+  console.log(groupedfooddata);
+
+  let foodItem = Object.keys(groupedfooddata).map((key => {
+    return (
+      <div key={key}>
+        <h1>
+          <Category Category_Name={key} />
+          
+        </h1>
+        {groupedfooddata[key].map((fooditem) => (
+          <RestaurantCategories
+            name={fooditem.name}
+            long_description={fooditem.long_description}
+            item_price={fooditem.price}
+            item_rating={"4.1"}
+            item_image={fooditem.image}
+          />
+        ))}
+      </div>
+    );
+  }));
+
   return (
     <div>
-      <div>{location.state.id}</div>
       <div className="container mt-0 ">
         <div className="d-sm-flex justify-content-center p-2 col">
-          <div>{DisplayData}</div>
+          {id}
+          <div>{!isLoading ? DisplayData : <ReactSpinner />}</div>
         </div>
-
-        <Category Category_Name={"Promotion"} />
+        <div>{foodItem}</div>
       </div>
     </div>
   );
--- viti-eats-frontend/src/home_component/RestaurantCategories.js
+++ viti-eats-frontend/src/home_component/RestaurantCategories.js
@@ -1,39 +1,65 @@
 import React from 'react'
 import filetburger from '../assets/filetburger.png'
 
-function RestaurantCategories({ Id, name, long_description, item_rating, item_price }) {
+function RestaurantCategories({ Id, name, long_description, item_rating, item_price,item_image }) {
+    item_image = "data:image/png;base64," + item_image;
     return (
-        <div className=''>
-           
-            <div class="row">
-                <div class="col-sm-6">
-                    <div class="card">
-                        <div class="card-body h-100">
-                            <div className='float-left w-25 h-50'> <img className='rounded img-fluid' src={filetburger} alt='' /></div>
-                            <div className='float-left w-75' style={{ fontWeight: "", fontSize: "18px", fontFamily: "" }}>
-                                <div className='float-left' style={{ fontWeight: "bold" }}>{name}</div>
-                                <br></br>
-                                <br></br>
-                                <div style={{ fontWeight: "lighter" }}>
-                                    <span className='float-left' style={{ fontSize: "14px", fontWeight: "lighter" }} >{long_description}</span>
-                                    <span className='float-right' style={{ fontSize: "14px", fontWeight: "lighter" }}>⭐{item_rating}</span>
-                                </div>
-
-                                <br></br>
-                                <div style={{ fontWeight: "lighter" }}>
-                                    <span className='float-left text-success' style={{ fontSize: "14px", fontWeight: "bold" }} >FJD$ {item_price}</span>
-                                    <span className='float-right' style={{ fontSize: "14px", fontWeight: "lighter" }}><button type="button" class="btn btn-danger">ADD</button>
-                                    </span>
-                                </div>
-
-
-                            </div>
-
-                        </div>
-                    </div>
+      <div className="">
+        <div class="row">
+          <div class="col-sm-6">
+            <div class="card">
+              <div class="card-body h-100">
+                <div className="float-left w-25 h-50">
+                  {" "}
+                  <img className="rounded img-fluid" src={item_image} alt="" />
                 </div>
+                <div
+                  className="float-left w-75"
+                  style={{ fontWeight: "", fontSize: "18px", fontFamily: "" }}
+                >
+                  <div className="float-left" style={{ fontWeight: "bold" }}>
+                    {name}
+                  </div>
+                  <br></br>
+                  <br></br>
+                  <div style={{ fontWeight: "lighter" }}>
+                    <span
+                      className="float-left"
+                      style={{ fontSize: "14px", fontWeight: "lighter" }}
+                    >
+                      {long_description}
+                    </span>
+                    <span
+                      className="float-right"
+                      style={{ fontSize: "14px", fontWeight: "lighter" }}
+                    >
+                      ⭐{item_rating}
+                    </span>
+                  </div>
 
-                {/* <div class="col-sm-6">
+                  <br></br>
+                  <div style={{ fontWeight: "lighter" }}>
+                    <span
+                      className="float-left text-success"
+                      style={{ fontSize: "14px", fontWeight: "bold" }}
+                    >
+                      FJD$ {item_price}
+                    </span>
+                    <span
+                      className="float-right"
+                      style={{ fontSize: "14px", fontWeight: "lighter" }}
+                    >
+                      <button type="button" class="btn btn-danger">
+                        ADD
+                      </button>
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* <div class="col-sm-6">
                     <div class="card">
                         <div class="card-body h-100">
                             <div className='float-left w-25 h-50'> <img className='rounded img-fluid' src={filetburger} alt='' /></div>
@@ -59,11 +85,10 @@ function RestaurantCategories({ Id, name, long_description, item_rating, item_pr
                         </div>
                     </div>
                 </div> */}
-
-            </div>
-            <div style={{ height: "20px" }}></div>
         </div>
-    )
+        <div style={{ height: "20px" }}></div>
+      </div>
+    );
 }
 
 export default RestaurantCategories
