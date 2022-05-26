9cd6d357dff5ffd9a54caf409d9e5a7aaa585f59
Deshant Singh <89639206+deshantDEV29@users.noreply.github.com>
2022-05-26 21:14:23 +1200

    Vendor Navigator update

--- viti-eats-frontend/src/order_retrieval/VendorHeader.js
+++ viti-eats-frontend/src/order_retrieval/VendorHeader.js
@@ -4,7 +4,7 @@ function VendorHeader() {
   return (
     <div>
       <div
-        className="h-25 d-flex justify-content-center bg-info position-sticky top-0 "
+        className="h-25 d-flex justify-content-center bg-success position-sticky top-0 "
         style={{ zIndex: "100" }}
       >
         <h1 className="text-center text-white">Viti Eats Vendor Protal</h1>
--- viti-eats-frontend/src/order_retrieval/VendorNavigator.js
+++ viti-eats-frontend/src/order_retrieval/VendorNavigator.js
@@ -2,15 +2,13 @@ import React from 'react'
 import "./VendorNavigator.css";
 import { Link } from "react-router-dom";
 import DashboardIcon from "@mui/icons-material/Dashboard";
-import RestaurantIcon from "@mui/icons-material/Restaurant";
+import TaskIcon from "@mui/icons-material/Task";
 import LogoutIcon from "@mui/icons-material/Logout";
-import MenuBookIcon from "@mui/icons-material/MenuBook";
-import FastfoodIcon from "@mui/icons-material/Fastfood";
 
 function VendorNavigator() {
   return (
     <div className=".col-xs-6 .col-md-4">
-      <Link to="/admin/" style={{ textDecoration: "none" }}>
+      <Link to="/vendor/" style={{ textDecoration: "none" }}>
         <div className="navigator__row">
           <div className="d-flex">
             <p>
@@ -20,13 +18,13 @@ function VendorNavigator() {
           </div>
         </div>
       </Link>
-      
-      <Link to="/admin/fooditem" style={{ textDecoration: "none" }}>
+
+      <Link to="/vendor/order" style={{ textDecoration: "none" }}>
         <div className="navigator__row">
           <p>
-            <FastfoodIcon />
+            <TaskIcon />
           </p>
-          <p>Food Item</p>
+          <p>Process Order</p>
         </div>
       </Link>
       <Link to="/logout" style={{ textDecoration: "none" }}>
